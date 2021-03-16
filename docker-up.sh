a840549428849d424c2ec11eb880e8485dc53198
Ryan Brock <ryan.brock@vizientinc.com>
2021-03-16 11:04:11 -0500

    make it compatible with docker

---
+++ docker-up.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+docker stop scorekeeping-by-rounds || true
+docker rm scorekeeping-by-rounds || true
+docker build -f Dockerfile -t scorekeeping-by-rounds .
+docker run -p 44200:80 --detach --name scorekeeping-by-rounds scorekeeping-by-rounds
